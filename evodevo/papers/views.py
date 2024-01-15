76e86a298b15ad2dfa2d6d746996fc0a90185b2a
Bruno Vellutini <organelas@gmail.com>
2024-01-15 08:43:49 +0100

    Make latest statuses list reverse chronological

--- evodevo/papers/views.py
+++ evodevo/papers/views.py
@@ -5,7 +5,7 @@ from papers.models import Status
 
 def home(request):
     sources = Source.objects.all()
-    statuses = Status.objects.filter(published=True)[:5]
+    statuses = Status.objects.filter(published=True).order_by('-created_at')[:5]
     context = {'sources': sources,
                'statuses': statuses,}
     return render(request, 'home.html', context)
