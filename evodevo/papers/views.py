c78f0edfd7ce43e0e7be27dc992c5891a7c966d5
Bruno Vellutini <organelas@gmail.com>
2024-11-10 22:02:47 +0100

    Empty papers app models and views

--- evodevo/papers/views.py
+++ evodevo/papers/views.py
@@ -1,10 +1,1 @@
 from django.shortcuts import render
-from bot.models import Feed, Post
-
-
-def home(request):
-    feeds = Feed.objects.order_by('-source__last_change')
-    posts = Post.objects.filter(published=True).order_by('-created_at')[:5]
-    context = {'feeds': feeds,
-               'posts': posts,}
-    return render(request, 'home.html', context)
