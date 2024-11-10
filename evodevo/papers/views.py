952df00b8b87cca03286c0cdff2fc3007a08236f
Bruno Vellutini <organelas@gmail.com>
2024-11-10 21:25:06 +0100

    Use new Post model instead of Status

--- evodevo/papers/views.py
+++ evodevo/papers/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
-from bot.models import Feed
-from papers.models import Status
+from bot.models import Feed, Post
 
 
 def home(request):
     feeds = Feed.objects.order_by('-source__last_change')
-    statuses = Status.objects.filter(published=True).order_by('-created_at')[:5]
+    posts = Post.objects.filter(published=True).order_by('-created_at')[:5]
     context = {'feeds': feeds,
-               'statuses': statuses,}
+               'posts': posts,}
     return render(request, 'home.html', context)
