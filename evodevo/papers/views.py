4564221f0c8cedf1a81a8cf31c04ffbec84ef3d6
Bruno Vellutini <organelas@gmail.com>
2024-11-10 20:11:15 +0100

    Show feed names with improved styling at home

--- evodevo/papers/views.py
+++ evodevo/papers/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-from feeds.models import Source
+from bot.models import Feed
 from papers.models import Status
 
 
 def home(request):
-    sources = Source.objects.all()
+    feeds = Feed.objects.order_by('-source__last_change')
     statuses = Status.objects.filter(published=True).order_by('-created_at')[:5]
-    context = {'sources': sources,
+    context = {'feeds': feeds,
                'statuses': statuses,}
     return render(request, 'home.html', context)
