5552ac731f0c2f86d1e8538583080e46de580141
Bruno Vellutini <organelas@gmail.com>
2025-06-14 23:27:09 +0200

    Make feed pages work for static website

--- evodevo/evodevo/urls.py
+++ evodevo/evodevo/urls.py
@@ -2,10 +2,15 @@ from django.contrib import admin
 from django.urls import path
 from django_distill import distill_path
 from bot.views import home, feeds, feed
+from bot.models import Feed
+
+def get_feeds():
+    for feed in Feed.objects.all():
+        yield {'id': feed.id}
 
 urlpatterns = [
     distill_path('', home, name='home'),
     distill_path('feeds/', feeds, name='feeds'),
-    distill_path('feed/<int:id>/', feed, name='feed'),
+    distill_path('feed/<int:id>/', feed, name='feed', distill_func=get_feeds),
     path('admin/', admin.site.urls),
 ]
