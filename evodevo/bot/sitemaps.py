69f5b7678f5d378310196d2377f3bdafcc28c38c
Bruno Vellutini <organelas@gmail.com>
2025-06-28 13:12:06 +0200

    Add feeds to sitemap

--- evodevo/bot/sitemaps.py
+++ evodevo/bot/sitemaps.py
@@ -3,8 +3,8 @@ from bot.models import Feed, Post
 
 
 class PostSitemap(Sitemap):
-    changefreq = "yearly"
-    priority = 0.5
+    changefreq = "weekly"
+    priority = 0.7
 
     def items(self):
         return Post.objects.filter(published=True).order_by("-created_at")
@@ -13,13 +13,24 @@ class PostSitemap(Sitemap):
         return obj.created_at
 
 
+class FeedSitemap(Sitemap):
+    changefreq = "monthly"
+    priority = 0.5
+
+    def items(self):
+        return Feed.objects.order_by("name")
+
+    # def lastmod(self, obj):
+        # return obj.created_at
+
+
 # Dictionary required for sitemaps
 sitemaps = {
     "sitemaps": {
         "posts": PostSitemap,
+        "feeds": FeedSitemap,
     }
 }
-
 
 
 
