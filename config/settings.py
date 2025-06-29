8213fd1a6eae4abc65eda0aa236eb405dabd3a8a
Bruno Vellutini <organelas@gmail.com>
2025-06-29 16:11:21 +0200

    Fix path to website static directory

--- config/settings.py
+++ config/settings.py
@@ -112,7 +112,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "website"
+STATIC_ROOT = BASE_DIR / "website/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
