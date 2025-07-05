ba436e0f23a25011bf5a7809ee12e4f000fa9894
Bruno Vellutini <organelas@gmail.com>
2025-07-05 22:40:07 +0200

    Create custom template tag to render statuses links

---
+++ bot/templatetags/custom_tags.py
@@ -0,0 +1,22 @@
+from django import template
+from django.utils.html import format_html
+from django.template.loader import render_to_string
+
+register = template.Library()
+
+@register.simple_tag
+def render_statuses_links(post):
+    html = []
+    statuses = post.statuses.filter(published=True).select_related('client')
+    for status in statuses:
+        icon_template = f"icon_{status.client.platform.lower()}.html"
+        icon_html = render_to_string(icon_template)
+        html.append(
+            format_html(
+                '<a href="{}" title="Post on {}">{}</a>',
+                status.url,
+                status.client.platform,
+                icon_html
+            )
+        )
+    return format_html("".join(str(h) for h in html))
