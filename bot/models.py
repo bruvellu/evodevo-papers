e9c72ec3d556ef533d03917f4c12a026e7bdf7c5
Bruno Vellutini <organelas@gmail.com>
2025-07-06 15:28:19 +0200

    Improve str method for post and status

--- bot/models.py
+++ bot/models.py
@@ -49,7 +49,7 @@ class Post(models.Model):
     link = models.URLField(blank=True)
 
     def __str__(self):
-        return f"Post[{self.id}]: {self.entry.title[:30]}..."
+        return f"[{self.id}] \"{self.entry.title[:50]}...\""
 
     def get_absolute_url(self):
         return reverse("post", args=(self.id,))
@@ -78,7 +78,7 @@ class Status(models.Model):
     is_published = models.BooleanField(default=False)
 
     def __str__(self):
-        return f"Status[{self.id}]: {self.post.title[:30]}..."
+        return f"id=[{self.id}] [{self.client}] \"{self.post.title[:50]}...\""
 
     def build_text(self, facets=False):
         # Faceted text object required for Bluesky
