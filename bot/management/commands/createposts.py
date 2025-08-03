eabb4127bb737945e766710efa62809dc25c6039
Bruno Vellutini <organelas@gmail.com>
2025-08-03 14:58:29 +0200

    Make createposts command more modular with functions

--- bot/management/commands/createposts.py
+++ bot/management/commands/createposts.py
@@ -8,11 +8,30 @@ class Command(BaseCommand):
     help = "Creates posts for new feed entries."
 
     def handle(self, *args, **options):
-        # Get entry IDs for existing posts
-        entries_with_a_post = Post.objects.values("entry_id")
+        """
+        Fetches new entries without posts, creates corresponding Post objects,
+        and outputs results to the command line.
+        """
+
+        new_entries = self.get_new_entries()
+        self.stdout.write(f"{new_entries.count()} new feed entries!")
+
+        for entry in new_entries:
+            post = self.create_post_from_entry(entry)
+            post.get_or_create_statuses()
+            self.stdout.write(self.style.SUCCESS(post))
+
+    def get_new_entries(self):
+        """
+        Returns a queryset of Entry objects that do not have
+        an associated Post. Ignore entries with specific terms.
+        Order entries by creation date (oldest first).
+        """
+        # Get the entry IDs of existing posts
+        entries_with_posts = Post.objects.values_list("entry_id", flat=True)
 
         # Exclude entries that already have a Post instance
-        entries_without_a_post = Entry.objects.exclude(id__in=entries_with_a_post)
+        entries_without_a_post = Entry.objects.exclude(id__in=entries_with_posts)
 
         # Exclude entries that are not articles (case insensitive)
         entries_without_a_post = entries_without_a_post.exclude(
@@ -22,15 +41,13 @@ class Command(BaseCommand):
         # Order by oldest to newest using the creation date
         entries_without_a_post = entries_without_a_post.order_by("created")
 
-        # Inform how many new entries will be added
-        self.stdout.write(f"{entries_without_a_post.count()} new feed entries!")
+        return entries_without_a_post
 
-        # Loop over new feed entries
-        for entry in entries_without_a_post:
-            # Create new post
-            post = Post(entry=entry, title=entry.title, link=entry.link)
-            post.save()
-            # Create linked statuses
-            post.get_or_create_statuses()
-            # Write output
-            self.stdout.write(self.style.SUCCESS(post))
+    def create_post_from_entry(self, entry):
+        """
+        Creates and saves a new Post object from a given Entry instance.
+        Returns the created Post object.
+        """
+        post = Post(entry=entry, title=entry.title, link=entry.link)
+        post.save()
+        return post
