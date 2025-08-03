ab6fe7288bfd200d9d0bee2d9a488076ef4a5410
Bruno Vellutini <organelas@gmail.com>
2025-08-03 15:10:38 +0200

    Separate filtering to an individual function

--- bot/management/commands/createposts.py
+++ bot/management/commands/createposts.py
@@ -7,6 +7,8 @@ from bot.models import Post
 class Command(BaseCommand):
     help = "Creates posts for new feed entries."
 
+    FILTERED_TITLE_TERMS = ["issue information", "front cover"]
+
     def handle(self, *args, **options):
         """
         Fetches new entries without posts, creates corresponding Post objects,
@@ -34,14 +36,21 @@ class Command(BaseCommand):
         entries_without_a_post = Entry.objects.exclude(id__in=entries_with_posts)
 
         # Exclude entries that are not articles (case insensitive)
-        entries_without_a_post = entries_without_a_post.exclude(
-            title__istartswith="issue information"
-        ).exclude(title__istartswith="front cover")
+        entries_without_a_post = self.filter_entries(entries_without_a_post)
 
         # Order by oldest to newest using the creation date
         entries_without_a_post = entries_without_a_post.order_by("created")
 
         return entries_without_a_post
+
+    def filter_entries(self, entries):
+        """
+        Exclude entries whose title contains any of the filtered terms (case-insensitive).
+        """
+        q = Q()
+        for term in self.FILTERED_TITLE_TERMS:
+            q |= Q(title__icontains=term)
+        return entries.exclude(q)
 
     def create_post_from_entry(self, entry):
         """
