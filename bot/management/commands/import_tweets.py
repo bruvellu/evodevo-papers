e68d466de744ebe78eb6d1f40381ba505a89bc85
Bruno Vellutini <organelas@gmail.com>
2025-08-06 22:30:33 +0200

    Create function converting created_at to datetime

--- bot/management/commands/import_tweets.py
+++ bot/management/commands/import_tweets.py
@@ -1,4 +1,5 @@
 import json
+from datetime import datetime
 
 from django.core.management.base import BaseCommand
 
@@ -12,20 +13,28 @@ class Command(BaseCommand):
         with open("tweets.js", "r", encoding="utf-8") as f:
             tweets = json.load(f)
 
+            self.stdout.write(f"Found {len(tweets)} tweets.")
+
             for tweet_data in tweets:
                 tweet = tweet_data["tweet"]
-                print(tweet["id"], tweet["created_at"])
+
+                id = tweet["id"]
+                created_at = self.get_created_at_datetime(tweet["created_at"])
+
+                print(id, created_at)
                 print(tweet["full_text"])
                 print()
 
-    def get_created_at_datetime(self):
+    def get_created_at_datetime(self, created_at):
         """Parse created_at timestamp to datetime object."""
-
-    def remove_hashtag(self):
-        """Remove #evodevo hashtag from full_text."""
+        # Example: "Thu Nov 03 05:19:51 +0000 2022"
+        return datetime.strptime(created_at, "%a %b %d %H:%M:%S %z %Y")
 
     def replace_shortened_by_expanded_url(self):
         """Replace t.co URL by true expanded URL in the full_text."""
+
+    def remove_hashtag(self):
+        """Remove #evodevo hashtag from full_text."""
 
     def determine_source_feed_from_expanded_url(self):
         """Discover the source by the paper's URL."""
