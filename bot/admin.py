3e78b9bb21a975cfde43c05d8480fb9437a22025
Bruno Vellutini <organelas@gmail.com>
2025-08-30 00:13:23 +0200

    Sort posts in admin by created timestamp

--- bot/admin.py
+++ bot/admin.py
@@ -31,6 +31,7 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ["entry__source__feed__name", "is_new", "created"]
     search_fields = ["title"]
     readonly_fields = ["created", "modified"]
+    ordering = ["-created"]
 
 
 class StatusAdmin(admin.ModelAdmin):
