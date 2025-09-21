9aa4a2c0acc20694e51669f16eb6f06b4acfcd9c
Bruno Vellutini <organelas@gmail.com>
2025-09-21 13:16:32 +0200

    Finalize command for parse and process legacy tweets

--- bot/management/commands/process_tweets.py
+++ bot/management/commands/process_tweets.py
@@ -1,17 +1,14 @@
 import json
 import os
 import time
-from configparser import DuplicateOptionError
 from datetime import datetime
 
 import requests
 from django.core.management.base import BaseCommand
 
-# TODO: Check entries with 403 status code
-
 
 class Command(BaseCommand):
-    help = "Import EvoDevo Papers legacy tweets."
+    help = "Parse and process legacy tweets from EvoDevo Papers."
 
     def handle(self, *args, **options):
         """Read, parse, and import the JSON file with backed-up tweets."""
@@ -21,17 +18,17 @@ class Command(BaseCommand):
         output_tweets = "processed_tweets.json"
         status_codes = {}
 
+        # Import already processed tweets
         if os.path.exists(output_tweets):
             with open(output_tweets, "r", encoding="utf-8") as f:
                 processed_tweets = json.load(f)
         else:
             processed_tweets = {}
 
+        # Import legacy tweets
         self.stdout.write(f"Reading {input_tweets}...")
-
         with open(input_tweets, "r", encoding="utf-8") as f:
             tweets = json.load(f)
-
         self.stdout.write(f"Found {len(tweets)} tweets.")
 
         # Sort tweets by creation date for consistency
@@ -85,6 +82,7 @@ class Command(BaseCommand):
             if tweet_out["resolved_url"]:
                 tweet_out["final_link"] = tweet_out["resolved_url"]
 
+            # Add tweet by ID
             processed_tweets[tweet_out["id"]] = tweet_out
 
             # Count status codes
@@ -93,31 +91,26 @@ class Command(BaseCommand):
             else:
                 status_codes[tweet_out["url_status_code"]] = 1
 
-            # created_at = self.get_created_at_datetime(tweet["created_at"])
-
-            print()
-            print(tweet_out["id"], tweet_out["created_at"])
-            print(f"{tweet_out["final_title"]} {tweet_out["final_link"]} #EvoDevo")
-
+            # Check shortened titles with ellipsis
             # if "…" in tweet_out["original_text"]:
             # print()
             # print(tweet_out["id"], tweet_out["created_at"])
             # print(f"{tweet_out["final_title"]} {tweet_out["final_link"]} #EvoDevo")
 
+            # Print out processed tweets for reference
+            print()
+            print(tweet_out["id"], tweet_out["created_at"])
+            print(f"{tweet_out["final_title"]} {tweet_out["final_link"]} #EvoDevo")
+
             # Write out processed tweets for persistent archive
             with open(output_tweets, "w", encoding="utf-8") as f:
                 json.dump(processed_tweets, f, ensure_ascii=False, indent=2)
 
+        # Summarize status codes
         print()
         print("Status codes")
         for k, v in status_codes.items():
             print(f"{k}: {v}")
-
-        # Parse processed tweets before import
-        posts_to_import = {}
-
-        with open(output_tweets, "r", encoding="utf-8") as f:
-            processed_tweets = json.load(f)
 
     def get_created_at_datetime(self, created_at):
         """Parse created_at timestamp to datetime object."""
@@ -133,9 +126,3 @@ class Command(BaseCommand):
     def replace_string(self, text, old, new):
         """Replace string in text."""
         return text.replace(old, new)
-
-    def determine_source_feed_from_expanded_url(self):
-        """Discover the source by the paper's URL."""
-
-    def generate_twitter_status_url(self):
-        """Build Twitter status URL from tweet ID."""
