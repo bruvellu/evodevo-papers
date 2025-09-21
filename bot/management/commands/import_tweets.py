9af49d280049990170b2d7b5d51d54b06713a107
Bruno Vellutini <organelas@gmail.com>
2025-09-21 21:44:37 +0200

    Base version for importing legacy tweets

--- bot/management/commands/import_tweets.py
+++ bot/management/commands/import_tweets.py
@@ -1,141 +1,97 @@
 import json
 import os
 import time
-from configparser import DuplicateOptionError
 from datetime import datetime
 
 import requests
 from django.core.management.base import BaseCommand
 
-# TODO: Check entries with 403 status code
+from bot.models import Feed, Post
 
 
 class Command(BaseCommand):
-    help = "Import EvoDevo Papers legacy tweets."
+    help = "Import legacy tweets to EvoDevo Papers database."
 
     def handle(self, *args, **options):
-        """Read, parse, and import the JSON file with backed-up tweets."""
+        """Read processed tweets and create posts."""
 
-        # Define input/output files and open
-        input_tweets = "tweets.json"
-        output_tweets = "processed_tweets.json"
-        status_codes = {}
-
-        if os.path.exists(output_tweets):
-            with open(output_tweets, "r", encoding="utf-8") as f:
+        # Read file of processed tweets
+        processed_tweets_file = "processed_tweets.json"
+        self.stdout.write(f"Reading {processed_tweets_file}...")
+        if os.path.exists(processed_tweets_file):
+            with open(processed_tweets_file, "r", encoding="utf-8") as f:
                 processed_tweets = json.load(f)
         else:
             processed_tweets = {}
+        self.stdout.write(f"Found {len(processed_tweets)} tweets.")
 
-        self.stdout.write(f"Reading {input_tweets}...")
+        # Create dictionary for import
+        posts = {}
+        duplicates = 0
 
-        with open(input_tweets, "r", encoding="utf-8") as f:
-            tweets = json.load(f)
+        # Loop over processed tweets
+        for tweet_id, tweet in processed_tweets.items():
+            if tweet["url_status_code"] == 404:
+                continue
 
-        self.stdout.write(f"Found {len(tweets)} tweets.")
-
-        # Sort tweets by creation date for consistency
-        sorted_tweets = sorted(
-            tweets,
-            key=lambda entry: self.get_created_at_datetime(
-                entry["tweet"]["created_at"]
-            ),
-        )
-
-        # Loop over sorted tweets
-        for entry in sorted_tweets:
-            # Tweet object with variables to parse and process
-            tweet_in = entry["tweet"]
-            tweet_out = {
-                "id": tweet_in["id"],
-                "created_at": tweet_in["created_at"],
-                "original_url": tweet_in["entities"]["urls"][0]["url"],
-                "expanded_url": tweet_in["entities"]["urls"][0]["expanded_url"],
-                "original_text": tweet_in["full_text"],
-                "resolved_url": "",
-                "url_response_ok": "",
-                "url_status_code": "",
-                "final_link": "",
-                "final_title": "",
+            # Define values for new post object
+            post = {
+                "entry": None,
+                "feed": self.discover_feed_from_link(tweet["final_link"]),
+                "title": tweet["final_title"],
+                "link": tweet["final_link"],
+                "created": self.get_created_at_datetime(tweet["created_at"]),
+                "is_new": True,
             }
-            tweet_tmp = processed_tweets[tweet_out["id"]]
 
-            # Resolve short URL stored as expanded_url
-            if not tweet_tmp["resolved_url"] or tweet_tmp["url_status_code"] == 404:
-                print(f"Resolving... {tweet_out['expanded_url']}")
-                response = self.resolve_url(tweet_out["expanded_url"])
-                tweet_out["resolved_url"] = response.url
-                tweet_out["url_status_code"] = response.status_code
-                tweet_out["url_response_ok"] = response.ok
-            else:
-                tweet_out["resolved_url"] = tweet_tmp["resolved_url"]
-                tweet_out["url_status_code"] = tweet_tmp["url_status_code"]
-                tweet_out["url_response_ok"] = tweet_tmp["url_response_ok"]
+            # Skip duplicate posts
+            if self.has_duplicate(post, posts):
+                print()
+                print("DUPLICATE", tweet["id"], tweet["created_at"])
+                print(tweet["final_title"])
+                print(tweet["final_link"])
+                duplicates += 1
+                continue
 
-            # Remove #evodevo hashtag from original text
-            text_no_hashtag = self.replace_string(
-                tweet_out["original_text"], " #evodevo", ""
-            )
-            # Remove original URL from text without hashtag
-            tweet_out["final_title"] = self.replace_string(
-                text_no_hashtag, f' {tweet_out["original_url"]}', ""
-            )
+            # Add post to be imported
+            posts[tweet_id] = post
 
-            # Replace the original URL with the resolved URL
-            if tweet_out["resolved_url"]:
-                tweet_out["final_link"] = tweet_out["resolved_url"]
-
-            processed_tweets[tweet_out["id"]] = tweet_out
-
-            # Count status codes
-            if tweet_out["url_status_code"] in status_codes.keys():
-                status_codes[tweet_out["url_status_code"]] += 1
-            else:
-                status_codes[tweet_out["url_status_code"]] = 1
-
-            # created_at = self.get_created_at_datetime(tweet["created_at"])
-
-            print()
-            print(tweet_out["id"], tweet_out["created_at"])
-            print(f"{tweet_out["final_title"]} {tweet_out["final_link"]} #EvoDevo")
-
-            # if "…" in tweet_out["original_text"]:
-            # print()
-            # print(tweet_out["id"], tweet_out["created_at"])
-            # print(f"{tweet_out["final_title"]} {tweet_out["final_link"]} #EvoDevo")
-
-            # Write out processed tweets for persistent archive
-            with open(output_tweets, "w", encoding="utf-8") as f:
-                json.dump(processed_tweets, f, ensure_ascii=False, indent=2)
-
+        # Print stats
         print()
-        print("Status codes")
-        for k, v in status_codes.items():
-            print(f"{k}: {v}")
-
-        # Parse processed tweets before import
-        posts_to_import = {}
-
-        with open(output_tweets, "r", encoding="utf-8") as f:
-            processed_tweets = json.load(f)
+        print(f"Posts: {len(posts.keys())}")
+        print(f"Duplicates: {duplicates}")
 
     def get_created_at_datetime(self, created_at):
         """Parse created_at timestamp to datetime object."""
         # Example: "Thu Nov 03 05:19:51 +0000 2022"
         return datetime.strptime(created_at, "%a %b %d %H:%M:%S %z %Y")
 
-    def resolve_url(self, url):
-        """Resolve URLs to their true address and get status."""
-        # Add delay to not overwhelm servers...
-        time.sleep(1)
-        return requests.head(url, allow_redirects=True, timeout=10)
-
-    def replace_string(self, text, old, new):
-        """Replace string in text."""
-        return text.replace(old, new)
-
-    def determine_source_feed_from_expanded_url(self):
-        """Discover the source by the paper's URL."""
+    def discover_feed_from_link(self, link):
+        """Discover the Feed object from the paper's link."""
+        if link.startswith("https://pubmed") or link.startswith("https://pmc"):
+            return Feed.objects.get(name="PubMed")
+        elif link.startswith("https://evodevojournal.biomedcentral.com"):
+            return Feed.objects.get(name="EvoDevo")
+        elif link.startswith("https://www.biorxiv.org"):
+            return Feed.objects.get(name="bioRxiv")
+        elif link.startswith("https://link.springer.com"):
+            return Feed.objects.get(name="Development Genes and Evolution")
+        elif link.startswith("https://onlinelibrary.wiley.com/doi/abs/10.1111/ede"):
+            return Feed.objects.get(name="Evolution & Development")
+        elif link.startswith("https://onlinelibrary.wiley.com/doi/abs/10.1002/jez"):
+            return Feed.objects.get(name="Journal of Experimental Zoology Part B")
+        elif link.startswith("https://www.frontiersin.org"):
+            return Feed.objects.get(name="Frontiers in Ecology and Evolution")
 
     def generate_twitter_status_url(self):
         """Build Twitter status URL from tweet ID."""
+
+    def has_duplicate(self, entry, posts):
+        """Check for duplicated entries before importing."""
+        for key, value in posts.items():
+            if (
+                entry["title"] == value["title"]
+                and entry["link"] == value["link"]
+            ):
+                return True
+        return False
